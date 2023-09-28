d27d967e2fd3b977dbc015f90778a28faec85ff6
Attila Reterics <reterics.attila@gmail.com>
2023-09-28 16:10:28 +0200

    Logs removed

--- src/components/gameContainer.tsx
+++ src/components/gameContainer.tsx
@@ -5,17 +5,12 @@ import {GameEngine} from "@/lib/gameEngine";
 import {NPC} from "@/lib/npc";
 
 export function GameContainer() {
-
     const reactCanvas = useRef(null);
-
 
     useEffect(() => {
         const { current: canvas } = reactCanvas;
         if (canvas) {
-            console.log(canvas);
             const game = new GameEngine(canvas as HTMLCanvasElement);
-            //
-            window.game = game;
             game.restartGame();
             const penguin = new NPC("penguin");
             penguin.setCustomCuts(0,0, 144, 128);
@@ -26,15 +21,12 @@ export function GameContainer() {
                     loadedSprite = true;
                     game.setPlayerSprite(penguin);
                 }
-
-
             }).catch(e=>{
-                console.log("error");
+                console.log(e);
             });
         }
     }, []);
 
-console.log('Render')
     return (
         <canvas ref={reactCanvas}/>
     )
